c32ca7cd46b220d45f7273eea2c9484029d7216f
Daniel <38920805+DaaanielBrandao@users.noreply.github.com>
2021-02-20 22:13:21 +0000

    skyyyyy aboveeeeeee

---
+++ Assets/Scripts/StarAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start () {
+        Animator anim = GetComponent<Animator>();
+ 
+        int pickAnumber = Random.Range(1,4);//exclusive never prints the last only goes 1 to 2
+        //Debug.Log (pickAnumber);
+ 
+//randJumpInt is the parameter in animator
+//pickAnumber random number from 1 to 2 from above
+        anim.SetInteger ("random", pickAnumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
