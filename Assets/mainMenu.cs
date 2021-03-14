f43fa7b9f0525f33794f512e159bc863e7a89b86
Daniel <38920805+DaaanielBrandao@users.noreply.github.com>
2021-03-14 23:19:56 +0000

    fixed menus :)

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -1,10 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    public GameObject defaultMain;
+    public GameObject defaultOptions;
+    private GameObject lastSelected;
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+            EventSystem.current.SetSelectedGameObject(lastSelected);
+        lastSelected = EventSystem.current.currentSelectedGameObject;
+    }
+
+    public void MainMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(defaultMain);
+        lastSelected = defaultMain;
+    }
+
+    public void OptionsMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(defaultOptions);
+        lastSelected = defaultOptions;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
