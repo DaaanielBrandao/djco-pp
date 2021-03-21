387bf7f2803e8e83fb7da3c4b2b8641a3ec1aeb9
MOAAS <miguel.moas@gmail.com>
2021-03-21 15:36:42 +0000

    BOSS

--- Assets/Scripts/Enemy/Behaviors/Follow.cs
+++ Assets/Scripts/Enemy/Behaviors/Follow.cs
@@ -1,164 +1,81 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using Pathfinding;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Follow : MovementBehavior
 {
     public float speed = 7.5f;
+    public float circleSpeed = 2f;
     public float minRange = 7.5f;
     public float maxRange = 10;
 
-    public float circleSpeed = 1.5f;
-    public int circleDir = 1;
-
+    public bool ignoreGround = false;
+    public bool ignorePlatforms = false;
+    
     public float visionRadius = 50f;
-
-    public bool goingAway = false; // :(
-   
-    private Vector2 originalScale;    
     private PlayerDetector detector;
-    private Animator animator;
-
-    private Path path;
-    private int currentWayPoint = 0;
-    public float nextWayPointDistance = 3f;
-    private bool reachedEndPath = false;
-
-    private Seeker seeker;
-    private Rigidbody2D rb;
+    private Vector2 originalScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        seeker = GetComponent<Seeker>();
-        rb = GetComponent<Rigidbody2D>();
-
-        originalScale = transform.localScale;
-
         // Ignore platforms
-        var objects = FindObjectsOfType<GameObject>();
-        foreach (var t in objects)
-            if (t.layer == PlatformCollision.PlatformLayer)
-                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), t.GetComponent<Collider2D>(), true);
+        GameObject[] objects = FindObjectsOfType<GameObject>();
+        Collider2D thisCollider = gameObject.GetComponent<Collider2D>();
+        foreach (var obj in objects)
+        {
+            if (ignoreGround && obj.layer == LayerMask.NameToLayer("Ground")) {
+                Physics2D.IgnoreCollision(thisCollider, obj.GetComponent<CompositeCollider2D>(), true);
+                Physics2D.IgnoreCollision(thisCollider, obj.GetComponent<TilemapCollider2D>(), true);
+            }
+            
+            if (ignorePlatforms && obj.layer == PlatformCollision.PlatformLayer)
+                Physics2D.IgnoreCollision(thisCollider, obj.GetComponent<Collider2D>(), true);
+        }
 
-        animator = GetComponent<Animator>();
+
         detector = gameObject.AddComponent<PlayerDetector>();
         detector.radius = visionRadius;
-        InvokeRepeating(nameof(UpdatePath),0f,.05f);
+        
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
-    void FixedUpdate() { 
+    void Update()
+    {
         GameObject follow = detector.nearestPlayer;
-        if (follow != null && Vector2.Distance(follow.transform.position, transform.position) <= visionRadius)
+        if (follow && Vector2.Distance(follow.transform.position, transform.position) <= visionRadius)
             FollowObject(follow);
-        else Wander();
-
-        AvoidOtherEnemies();
+  
     }
 
-    void UpdatePath()
+    private void FollowObject(GameObject follow)
     {
-        if(detector.nearestPlayer != null && seeker.IsDone())
-        {
-           // Debug.Log(detector.nearestPlayer.transform.position);
-            seeker.StartPath(rb.position, detector.nearestPlayer.transform.position, OnPathComplete);
-        }
-    }
-
-    void OnPathComplete(Path p)
-    {
-        if (!p.error)
-        {
-            //Debug.Log("newpath!");
-            path = p;
-            currentWayPoint = 0;
-        }
-    }
-    
-    void AvoidOtherEnemies() {
-        CircleCollider2D collider = GetComponent<CircleCollider2D>();
-        float colliderRadius = collider.bounds.extents.x;
-
-        LayerMask mask = LayerMask.GetMask(new string[]{"Enemy"});
-        Collider2D[] hits = Physics2D.OverlapCircleAll(collider.bounds.center, colliderRadius + 2, mask);
-
-        foreach (Collider2D hit in hits) {
-            if (hit.gameObject != gameObject) {
-                if (transform.position.x > hit.gameObject.transform.position.x) {
-                    StartCoroutine(GoAway());
-                }
-                break;
-            }
-        }
-    }
-
-    IEnumerator GoAway() {
-        goingAway = true;
-        yield return new WaitForSeconds(1.0f);
-        goingAway = false;
-    }
-
-    void Wander() {
-       // float x = (Mathf.PerlinNoise(transform.position.x, transform.position.y) - 0.5f);
-       // float y = (Mathf.PerlinNoise(transform.position.y, transform.position.x) - 0.5f);
-       // Debug.Log("PERLIN: " + x + "  " + y);
-       // transform.Translate(new Vector3(x, y));
-    }
-
-    void FollowObject(GameObject follow)
-    {
-        
-        Vector2 dir;
-        if (path == null)
-        {
-            return;
-        }
-        if (currentWayPoint >= path.vectorPath.Count)
-        {
-            reachedEndPath = true;
-            return;
-
-        }
-        
-        reachedEndPath = false;
-        dir = ((Vector2) path.vectorPath[Math.Min(currentWayPoint + 5, path.vectorPath.Count - 1)] - rb.position).normalized;
-        float distanceToWaypoint = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
-        //Debug.Log("wapoint: " + currentWayPoint + " " + dir + "distancetoway: " + distanceToWaypoint);
-        
-        if (distanceToWaypoint < nextWayPointDistance)
-        {
-            currentWayPoint++;
-        }
+        Vector2 dir = follow.transform.position - gameObject.transform.position;
 
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        float distance =  (follow.transform.position - gameObject.transform.position).magnitude;
-       // Debug.Log("Distance: " + distance);
-        //float distance = dir.magnitude;
         
-        if (distance == 0 && minRange > 0) // Edge case
+        float distance = dir.magnitude;
+        
+        if (distance == 0) // Edge case
             dir = new Vector2(0, 1);
-
+        
         // Move
         if (distance > maxRange) { // Towards player
-            Debug.Log("chasing");
+           // Debug.Log(distance + "b");
+            dir = dir.normalized;
             dir.x *= 1.5f;
-
         }
-        else if (distance < minRange)
-        {
-            // Away from player
-            dir = -dir/ 2;
-            Debug.Log("awaying");
+        else if (distance < minRange) { // Away from player
+            dir = -dir.normalized / 2; 
+           // Debug.Log(distance + "m");
         }
-
-        Debug.Log(dir.magnitude);
-        //Vector2 force = dir * speed * Time.deltaTime;
-        //rb.AddForce(force);
+        else {
+            dir = Vector2.zero;
+        }
         
-        transform.Translate(dir * speed * Time.deltaTime, Space.World);
+        transform.Translate(dir * (speed * Time.deltaTime), Space.World);
         
         // Rotate
         if (angle > 90 || angle < -90) {
@@ -169,19 +86,10 @@ public class Follow : MovementBehavior
             transform.localScale = originalScale;
             transform.rotation = Quaternion.Euler(0, 0, angle);
         }
-
+        
         // Circle
-        //transform.RotateAround(follow.transform.position, Vector3.forward, circleDir * circleSpeed * 360 / distance * Time.deltaTime);
-    }
-
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
-            circleDir = -circleDir;
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        //Gizmos.DrawSphere (path.vectorPath[Math.Min(currentWayPoint + 5, path.vectorPath.Count - 1)],1);
+        if (dir == Vector2.zero)
+            transform.RotateAround(follow.transform.position, Vector3.forward, circleSpeed * 360 / distance * Time.deltaTime);
+        
     }
 }
